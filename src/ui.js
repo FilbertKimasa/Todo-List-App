349dcc50dac664be40cc16e1f71fee29a286071f
FilbertKimasa <filbertdan67@gmail.com>
2023-08-26 23:01:28 +0300

    Modify ui.js file

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,5 @@
+import Data from './data';
+
 export default class UI {
   constructor() {
     this.textInput = document.querySelector('.form-input');
@@ -6,9 +8,16 @@ export default class UI {
     this.taskList = document.querySelector('.task-list');
     this.form = document.querySelector('.task-form');
     this.sync = document.querySelector('.sync');
+
+    this.sync.addEventListener('click', this.syncPage.bind(this));
   }
 
   clearInputs() {
     this.textInput.value = '';
+  }
+
+  syncPage() {
+    window.location.reload();
+    this.textInput.focus();
   }
 }
