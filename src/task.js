c29480418338b98563218d1b38372f23ec57f2d0
FilbertKimasa <filbertdan67@gmail.com>
2023-08-27 23:08:10 +0300

    Add addTask

--- src/task.js
+++ src/task.js
@@ -5,8 +5,31 @@ export default class Task {
   constructor() {
     this.ui = new UI();
     this.data = new Data();
+    this.ui.formBtn.addEventListener('click', this.addTask.bind(this));
     this.ui.textInput.focus();
     this.renderTasks();
+  }
+
+  addTask() {
+    this.ui.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+    const description = this.ui.textInput.value.trim();
+    const index = this.data.tasks.length;
+    const completed = false;
+
+    if (description.length > 0) {
+      const task = { index, description, completed };
+      const same = this.data.tasks.some(
+        (tsk) => JSON.stringify(tsk) === JSON.stringify(task)
+      );
+      if (!same) {
+        this.data.tasks.push(task);
+        this.data.updateStorage();
+        this.ui.clearInputs();
+        this.renderTasks();
+      }
+    }
   }
 
   renderTasks() {
@@ -18,8 +41,18 @@ export default class Task {
             <label for="0" class="task-description">${task.description}</label
             ><button type="button" class="vertical-dots">&#8942;</button>
           </li>
-        `,
+        `
       )
       .join('');
+
+    const removeBtns = document.querySelectorAll('.vertical-dots');
+    removeBtns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const card = e.target.closest('.task');
+        const taskName = card.querySelector('.task-description').innerText;
+        this.data.removeTask(taskName);
+        this.renderTasks();
+      });
+    });
   }
 }
