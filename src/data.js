be6484a9f394450491bdf7c8eb99f453408bf5e4
FilbertKimasa <filbertdan67@gmail.com>
2023-08-27 23:14:36 +0300

    Add storage manipulation functionality

--- src/data.js
+++ src/data.js
@@ -1,16 +1,21 @@
 export default class Data {
   constructor() {
-    this.tasks = [
-      {
-        description: 'An example description one',
-        completed: false,
-        index: 1,
-      },
-      {
-        description: 'An example description two',
-        completed: false,
-        index: 2,
-      },
-    ];
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  }
+
+  removeTask(taskName) {
+    this.tasks = this.tasks.filter((task) => task.description !== taskName);
+    this.updateStorage();
+  }
+
+  updateStorage() {
+    if (this.tasks) {
+      for (let i = 0; i < this.tasks.length; i += 1) {
+        this.tasks[i].index = i;
+      }
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      return;
+    }
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
