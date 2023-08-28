f5d43bcdd6e65d3145043be42e67c048901f8d62
FilbertKimasa <filbertdan67@gmail.com>
2023-08-29 00:37:45 +0300

    Modify task.js file

--- src/task.js
+++ src/task.js
@@ -6,6 +6,10 @@ export default class Task {
     this.ui = new UI();
     this.data = new Data();
     this.ui.formBtn.addEventListener('click', this.addTask.bind(this));
+    this.ui.clearCompleted.addEventListener(
+      'click',
+      this.clearCompleted.bind(this)
+    );
     this.ui.textInput.focus();
     this.renderTasks();
   }
@@ -21,7 +25,7 @@ export default class Task {
     if (description.length > 0) {
       const task = { index, description, completed };
       const same = this.data.tasks.some(
-        (tsk) => JSON.stringify(tsk) === JSON.stringify(task),
+        (tsk) => JSON.stringify(tsk) === JSON.stringify(task)
       );
       if (!same) {
         this.data.tasks.push(task);
@@ -37,23 +41,47 @@ export default class Task {
       .map(
         (task) => `
           <li class="task" id="${task.index}">
-            <input type="checkbox" name="" value="pp" />
+            <input type="checkbox" name="" ${
+              task.completed ? 'checked' : ''
+            } class="task-state" />
             <label for="0" class="task-description">${task.description}</label
-            ><button type="button" class="vertical-dots">&#8942;</button>
+            ><button type="button" class="vertical-dots"><span class="three-dots">&#8942;</span><i class="material-icons delete-icon">delete</i></button>
           </li>
-        `,
+        `
       )
       .join('');
 
     const removeBtns = document.querySelectorAll('.vertical-dots');
+    const taskState = document.querySelectorAll('.task-state');
     removeBtns.forEach((btn) => {
       btn.addEventListener('click', (e) => {
         const card = e.target.closest('.task');
         let { id } = card;
         id = parseInt(id, 10);
         this.data.removeTask(id);
-        this.renderTasks(id);
+        this.renderTasks();
       });
     });
+
+    taskState.forEach((check) => {
+      check.addEventListener('click', (e) => {
+        const card = e.target.closest('.task');
+        let { id } = card;
+        id = parseInt(id, 10);
+        const eventState = card.querySelector('.task-state').checked;
+        this.data.updateTaskStatus(id, eventState);
+        this.renderTasks();
+      });
+    });
+  }
+
+  clearCompleted() {
+    const tasksList = document.querySelectorAll('.task-state');
+    tasksList.forEach((task) => {
+      if (task.checked) {
+        this.data.removeCompletedTasks(task.checked);
+      }
+    });
+    this.renderTasks();
   }
 }
